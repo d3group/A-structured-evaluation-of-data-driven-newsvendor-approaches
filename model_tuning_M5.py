ebdd545736b5cb28be7f3b3396e15c7b5ea77cae
AndreasPhilippi <50919805+AndreasPhilippi@users.noreply.github.com>
2021-07-19 14:49:46 +0200

    Update model_tuning_M5.py

--- model_tuning_M5.py
+++ model_tuning_M5.py
@@ -1,28 +1,3 @@
-from sklearn.utils.validation import check_array
-from sklearn.model_selection import ParameterGrid, GridSearchCV, RepeatedKFold, KFold
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import make_scorer
-from ddop.metrics import average_costs, prescriptiveness_score
-from ddop.newsvendor import SampleAverageApproximationNewsvendor
-from ddop.newsvendor import DecisionTreeWeightedNewsvendor
-from ddop.newsvendor import RandomForestWeightedNewsvendor 
-from ddop.newsvendor import KNeighborsWeightedNewsvendor
-from ddop.newsvendor import LinearRegressionNewsvendor
-from ddop.newsvendor import GaussianWeightedNewsvendor
-from ddop.newsvendor import LinearRegressionNewsvendor
-from ddop.newsvendor import DeepLearningNewsvendor
-import numpy as np
-import pandas as pd
-import time
-import os
-import logging
-import statistics
-from multiprocessing import Pool
-from joblib import Parallel, delayed
-from sklearn.base import clone
-
 # a function  to create and save logs in the log files
 def log(path, file):
     """[Create a log file to record the experiment's logs]
@@ -79,9 +54,8 @@ def get_sl_scores(X, y, params, cu, co, estimator, cv):
             scores.append(score)
         sl_scores.append(scores)
         scores = []
-    mean_sl_scores = np.array(sl_scores).mean(axis=0)
         
-    return mean_sl_scores
+    return sl_scores
 
 
 def main():
@@ -89,13 +63,12 @@ def main():
     # define file paths    
     log_path = ""
     result_path = ""
-    data_path = "m5.csv"
     
     # set a logger file
     logger = log(path=log_path, file="cross_val.logs")
     
-    # load data
-    data = pd.read_csv(data_path)
+    # load data (data available in team drive space)
+    data = pd.read_csv("M5_final.csv")
     X = data.drop(["demand"], axis=1)
     y = pd.DataFrame(data["demand"])
     
@@ -116,9 +89,9 @@ def main():
                       (2*n_features,2*1*n_features),
                       (3*n_features,round(3*0.5*n_features)),
                       (3*n_features,3*1*n_features)],
-          "epochs": [10,100,200]}
+          "epochs": [100]}
 
-    dtw = {"max_depth":[None,2,4,6,8,10],
+    dtw = {"max_depth":[None,2,4,6,8,10]
            "min_samples_split": [2,4,6,8,16,32,64]
           }
 
@@ -127,7 +100,6 @@ def main():
               'n_estimators':[10,20,50,100]}
 
     knnw = {'n_neighbors':[1,2,4,8,16,32,64,128]}
-    #knnw = {'n_neighbors':[16,32]}
 
     gkw = {'kernel_bandwidth':[1,1.5,2,2.5,3, *np.arange(3.5, n_features+0.5, 0.5)]}
     
@@ -136,38 +108,26 @@ def main():
     estimator_tuple_list.append(('SAA', SampleAverageApproximationNewsvendor(),None))
     estimator_tuple_list.append(('LR', LinearRegressionNewsvendor(),None))
     estimator_tuple_list.append(('DTW', DecisionTreeWeightedNewsvendor(random_state=1),dtw))
-    estimator_tuple_list.append(('RFW', RandomForestWeightedNewsvendor(random_state=1),rfw))
-    estimator_tuple_list.append(('KNNW',KNeighborsWeightedNewsvendor(),knnw))
-    estimator_tuple_list.append(('GKW', GaussianWeightedNewsvendor(),gkw))
+    #estimator_tuple_list.append(('RFW', RandomForestWeightedNewsvendor(random_state=1),rfw))
+    #estimator_tuple_list.append(('KNNW',KNeighborsWeightedNewsvendor(),knnw))
+    #estimator_tuple_list.append(('GKW', GaussianWeightedNewsvendor(),gkw))
     #estimator_tuple_list.append(('DL', DeepLearningNewsvendor(),dl))
     
     # define under- and overage costs
     cu = [9, 7.5, 5, 2.5, 1]
     co = [1, 2.5, 5, 7.5, 9]
     
-    best_model = pd.DataFrame()
-    
-    # create dataframe to write results in 
-    estimator_names = [] 
-    for estimator_tuple in estimator_tuple_list:
-        estimator_names.append(estimator_tuple[0])
-    estimator_names = pd.DataFrame(data={'Model': estimator_names})
-    estimator_names["index"] = 1
-    SL = pd.DataFrame()
-    SL["cu"] = cu
-    SL["co"] = co
-    SL["SL"] = SL["cu"]/(SL["cu"]+SL["co"])
-    SL["index"] = 1
-    results = pd.merge(SL, estimator_names, on="index", how="right")
-    results.drop(["index"], inplace = True, axis=1)
-    for group in groups:
-        results[str(group)+ "AC"] = np.nan
-        results[str(group)+ "SoP"] = np.nan
+    # create dataframe to write best results in 
+    results = pd.DataFrame()
+     
+    # dataframe for cv results
+    cv_results = pd.DataFrame()
     
     #define cv strategy
-    cv = KFold(n_splits=10)
+    n_splits = 10
+    cv = KFold(n_splits=n_splits)
     
-
+    
     for  estimator_tuple in estimator_tuple_list:
         for group in groups:
 
@@ -200,8 +160,11 @@ def main():
                     pred = estimator.predict(X_test.shape[0])
                     pred = scaler_target.inverse_transform(pred)
                     avg_costs = average_costs(y_test,pred,cu_i,co_i)
-                    results.loc[(results["Model"]==estimator_name) & (results["cu"]==cu_i), str(group)+ "AC"]=round(avg_costs,3)
-                    results.loc[(results["Model"]==estimator_name) & (results["cu"]==cu_i), str(group)+ "SoP"]=0
+                    avg_costs = round(avg_costs,4)
+                    
+                    d = {'Model': estimator_name, 'cu': cu_i, 'co': co_i, 'SL': cu_i/(cu_i+co_i), 'Group': str(group), 'Average costs': avg_costs, 'Coefficient of Prescriptiveness': 0, 'Best Params': np.nan}
+                    results = results.append(d, ignore_index=True)
+                    
                     
             elif estimator_name in ["KNNW", "RFW", "DTW", "GKW"]:
                 logger.info('Parameter tuning {} for group {}'.format(estimator_name, group))
@@ -212,9 +175,19 @@ def main():
 
                 scores = parallel(delayed(get_sl_scores)(X=X_train, y=y_train, params=params, cu=cu, co=co, estimator=estimator, cv=cv) for params in candidate_params)
                 scores = np.array(scores)
-                rank_scores = scores.argmax(axis=0)
-                best_scores = scores.max(axis=0)
+                mean_scores = scores.mean(axis=0)
+                rank_scores = mean_scores.argmax(axis=0)
+                best_scores = mean_scores.max(axis=0)
                 best_params = [candidate_params[rank] for rank in rank_scores]
+                
+                for i in range(len(cu)):
+                    cv_results_temp = pd.DataFrame(scores.T[0].T, columns = ['split'+str(split)+'_test_score' for split in range(n_splits)])
+                    cv_results_temp["Model"] = estimator_name
+                    cv_results_temp["cu"] = cu[i]
+                    cv_results_temp["co"] = co[i]
+                    cv_results_temp["SL"] = cu[i]/(cu[i]+co[i])
+                    cv_results_temp["param"] = candidate_params
+                    cv_results = pd.concat([cv_results, cv_results_temp], ignore_index=True)
 
                 for i in range(len(cu)):
                     logger.info('Train {} with best params for group {} and service level {}'.format(estimator_name, group, cu[i]/(co[i]+cu[i])))
@@ -224,16 +197,15 @@ def main():
                     pred = best_estimator.predict(X_test)
                     pred = scaler_target.inverse_transform(pred)
                     avg_costs = average_costs(y_test,pred,cu[i],co[i])
-                    results.loc[(results["Model"]==estimator_name) & (results["cu"]==cu[i]), str(group)+ "AC"]=round(avg_costs,3)
+                    avg_costs = round(avg_costs,4)
                     
                     saa_pred = SampleAverageApproximationNewsvendor(cu[i],co[i]).fit(y_train).predict(X_test.shape[0])
                     saa_pred = scaler_target.inverse_transform(saa_pred)
                     SoP = prescriptiveness_score(y_test, pred, saa_pred, cu[i], co[i])
-                    results.loc[(results["Model"]==estimator_name) & (results["cu"]==cu[i]), str(group)+ "SoP"]=round(SoP,3)
+                    SoP = round(SoP,4)
                     
-                    g = {'SL': [cu[i]/(cu[i]+co[i])], 'group' : str(group), 'model_type' : estimator_name, 'model': best_estimator}
-                    best_model = pd.concat([best_model, pd.DataFrame(data=g)])
-                    best_model.to_pickle(result_path+'best_models.csv')  
+                    d = {'Model': estimator_name, 'cu': cu[i], 'co': co[i], 'SL': cu[i]/(cu[i]+co[i]), 'Group': str(group), 'Average costs': avg_costs, 'Coefficient of Prescriptiveness': SoP, 'Best Params': best_params[i]}
+                    results = results.append(d, ignore_index=True)
                     
                     logger.info("Average cost of best model: {}".format(avg_costs))
                     logger.info("Coeff of Prescriptiveness of best model: {}".format(SoP))
@@ -247,12 +219,15 @@ def main():
                     pred = estimator.predict(X_test)
                     pred = scaler_target.inverse_transform(pred)
                     avg_costs = average_costs(y_test,pred,cu_i,co_i)
-                    results.loc[(results["Model"]==estimator_name) & (results["cu"]==cu_i), str(group)+ "AC"]=round(avg_costs,3)
-                    
+                    avg_costs = round(avg_costs,4)
+                                        
                     saa_pred = SampleAverageApproximationNewsvendor(cu_i,co_i).fit(y_train).predict(X_test.shape[0])
                     saa_pred = scaler_target.inverse_transform(saa_pred)
                     SoP = prescriptiveness_score(y_test, pred, saa_pred, cu_i, co_i)
-                    results.loc[(results["Model"]==estimator_name) & (results["cu"]==cu_i), str(group)+ "SoP"]=round(SoP,3)
+                    SoP = round(SoP,4)
+                    
+                    d = {'Model': estimator_name, 'cu': cu_i, 'co': co_i, 'SL': cu_i/(cu_i+co_i), 'Group': str(group), 'Average costs': avg_costs, 'Coefficient of Prescriptiveness': SoP, 'Best Params': np.nan}
+                    results = results.append(d, ignore_index=True)
                     
                     logger.info("Average cost of best model: {}".format(avg_costs))
                     logger.info("Coeff of Prescriptiveness of best model: {}".format(SoP))
@@ -262,20 +237,42 @@ def main():
                 for cu_i, co_i in zip(cu,co):
                     base_estimator = clone(estimator)
                     base_estimator.set_params(cu=cu_i,co=co_i)
+                    logger.info('Parameter tuning {} for group {} and SL {}'.format(estimator_name, group, cu_i/(co_i+cu_i)))
                     gs = GridSearchCV(base_estimator, param_grid, cv=cv, n_jobs=-1)
                     gs.fit(X_train,y_train)
+                    
+                    cv_results_temp = pd.DataFrame({k: v for k, v in gs.cv_results_.items() if k.startswith('split')})
+                    cv_results_temp["Group"] = str(group)
+                    cv_results_temp["Model"] = estimator_name
+                    cv_results_temp["cu"] = cu_i
+                    cv_results_temp["co"] = co_i
+                    cv_results_temp["SL"] = cu_i/(cu_i+co_i)
+                    cv_results_temp["param"] = gs.cv_results_["params"]
+                    cv_results = pd.concat([cv_results, cv_results_temp], ignore_index=True)
+                    
                     best_estimator = gs.best_estimator_
                     pred = best_estimator.predict(X_test)
                     pred = scaler_target.inverse_transform(pred)
                     avg_costs = average_costs(y_test,pred,cu_i,co_i)
-                    results.loc[(results["Model"]==estimator_name) & (results["cu"]==cu_i), str(group)+ "AC"]=round(avg_costs,3)
+                    avg_costs = round(avg_costs,4)
 
                     saa_pred = SampleAverageApproximationNewsvendor(cu_i,co_i).fit(y_train).predict(X_test.shape[0])
                     saa_pred = scaler_target.inverse_transform(saa_pred)
                     SoP = prescriptiveness_score(y_test, pred, saa_pred, cu_i, co_i)
-                    results.loc[(results["Model"]==estimator_name) & (results["cu"]==cu_i), str(group)+ "SoP"]=round(SoP,3)
+                    SoP = round(SoP,4)
+                    
+                    d = {'Model': estimator_name, 'cu': cu_i, 'co': co_i, 'SL': cu_i/(cu_i+co_i), 'Group': str(group), 'Average costs': avg_costs, 'Coefficient of Prescriptiveness': SoP, 'Best Params': gs.best_params_}
+                    results = results.append(d, ignore_index=True)
+                    
+                    logger.info("Average cost of best model: {}".format(avg_costs))
+                    logger.info("Coeff of Prescriptiveness of best model: {}".format(SoP))
+                    logger.info("------------------------------------------------------------------")
                     
             results.to_csv(result_path+'results.csv', index=False)
+            cv_results.to_csv(result_path+'cv_results.csv', index=False)
+            
+            # break for testing --> consinder only one group. Remove for final testing !!!!!!
+            break
             
 
 if __name__ == '__main__':
